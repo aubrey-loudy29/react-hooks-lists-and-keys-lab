f6cb0caa9bc75fc87c920da4481129bf15e7610d
Aubrey Loudenslager <aubrey.d.webster@gmail.com>
2023-03-14 13:42:33 -0700

    final

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,16 @@
 import React from "react";
+import user from "../data/user"
 
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return (
+    <nav>
+      <a href="#home" key={user.city}>home</a>
+      <a href="#about" key={user.bio}>about</a>
+      <a href="#projects" key={user.projects}>projects</a>
+    </nav>
+  )
 }
 
 export default NavBar;
